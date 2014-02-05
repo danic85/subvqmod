8888875570b19a1fe07ae5c4a9cf0a9f7dd29b8a
Dan Nicholson <github@dannicholson.co.uk>
2014-02-05 10:45:29 +0000

    Create SimpleXMLExtended.php

---
+++ SimpleXMLExtended.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package SubVQMod
+ * @version 0.1.0
+ * @author Dan Nicholson
+ * @license https://github.com/danic85/subvqmod/blob/master/LICENSE - MIT Licence (MIT)
+ * @link   http://dannicholson.co.uk/projects/subvqmod
+ */
+class SimpleXMLExtended extends SimpleXMLElement {
+	/**
+	 * Add CData Text to XML Element
+	 * @param string $text CDataText
+	 */
+	public function addCData($text) {
+		$node = dom_import_simplexml($this);
+		$od   = $node->ownerDocument;
+		$node->appendChild($od->createCDATASection($text));
+	}
+}
